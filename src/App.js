20bce8527ad8d97c7263526a36ddba7f0f436f24
abdelkarim <abdelkarimbenchelef.1361@gmail.com>
2023-02-17 11:44:45 +0100

    Router to define where app want have to access, Routes to define all Route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router,  Route, Routes} from 'react-router-dom';
 import { Home } from './pages/Home';
+import { Menu } from './pages/Menu';
+import { Contact } from './pages/Contact';
 
 function App() {
   return (
     <div className="App">
         <Router>
-           <Routes>
+               {/* <div>
+                <Link></Link>
+               </div> */}
+
+              <Routes>
                <Route path="/" element={<Home />}/>
-           </Routes>
+               <Route path="/menu" element={<Menu />}/>
+               <Route path="/contact" element={<Contact />}/>
+               <Route path="*" element={<h1>page not found</h1>}/>
+              </Routes>
         </Router>
     </div>
   );
